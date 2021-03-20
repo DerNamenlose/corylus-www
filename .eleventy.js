f0ad02bdaede8156918f696d8994baef6972391e
Markus Brueckner <namenlos@geekbetrieb.de>
2021-03-20 17:30:18 +0100

    - create build action for GitHub

--- .eleventy.js
+++ .eleventy.js
@@ -2,13 +2,4 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('img');
     eleventyConfig.addPassthroughCopy('fonts');
     eleventyConfig.setTemplateFormats(['md', 'css']);
-
-    const markdownIt = require('markdown-it');
-    const markdownItEmoji = require('markdown-it-emoji');
-    const options = {
-        html: true,
-    };
-    const markdownLib = markdownIt(options).use(markdownItEmoji);
-
-    eleventyConfig.setLibrary('md', markdownLib);
 };
